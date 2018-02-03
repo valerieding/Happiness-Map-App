939ca994965eafeb033c05520d0156b2069c5c6f
Valerie Ding <valerieding@cs.uchicago.edu>
2018-02-03 11:50:43 -0600

    Working on tests for posting API

--- src/db/message_api_test.py
+++ src/db/message_api_test.py
@@ -28,11 +28,15 @@ class MessageAPITest(unittest.TestCase):
         self.assertEquals(set1, set2)
 
     def test_add_post(self):
-        self.votingApi.add_vote(123, self.loc1, 3)
-        self.votingApi.add_vote(456, self.loc2, 5)
-        vote_id = (self.db.execute("SELECT * FROM votes WHERE uid = ?", (123,)))[0][0]
-        self.messageApi.add_post(123, self.loc1, "hi! i'm user 123 at location 1, adding my first post")
-        self.messageApi.add_post(456, self.loc2, "hi! i'm user 234 at location 2, adding my first post")
+        self.votingApi.add_vote(1, self.loc1, 3)
+        self.votingApi.add_vote(2, self.loc2, 5)
+        self.votingApi.add_vote(3, self.loc3, 1)
+        vote_id1 = (self.db.execute("SELECT * FROM votes WHERE uid = ?", (1,)))[0][0]
+        vote_id2 = (self.db.execute("SELECT * FROM votes WHERE uid = ?", (2,)))[0][0]
+        vote_id3 = (self.db.execute("SELECT * FROM votes WHERE uid = ?", (3,)))[0][0]
+        self.messageApi.add_post(1, self.loc1, "hi! i'm user 1 at location 1, adding my first post")
+        self.messageApi.add_post(2, self.loc2, "hi! i'm user 2 at location 2, adding my first post")
+        self.messageApi.add_post(2, self.loc3, "hi! i'm user 3 at location 3, adding my first post")
 
 
 if __name__ == '__main__':
