cb60ff7f625e24219b7fc3c0f0d67cd59502d089
Claire Dolin <cdolin@uchicago.edu>
2018-02-08 22:34:56 -0600

    drafts of tests for map page

--- src/map/mmap.js
+++ src/map/mmap.js
@@ -1,10 +1,10 @@
-var rsr = Raphael('map', '1458', '1076.67'); 
+var rsr = Raphael('map', '1458', '1076.67');
 
 var regions = [];
 
 var ColorKey = Object.freeze({
-	happiest:"#182825", 
-	happy:"#CAE7B9", 	
+	happiest:"#182825",
+	happy:"#CAE7B9",
 	neutral:"#E8E1EF",
 	sad: "#D0B8AC",
 	saddest: "#721817"
@@ -15,49 +15,49 @@ var ColorKey = Object.freeze({
 var dic = {
 	"mansueto": {name: "Mansueto Library", val: "happiest"},
 	"regenstein": {name: "Regenstein Library", val: "saddest"},
-	"bartlett": {name: "Barlett Dining Commons", val: "happy"},
-	"maxp": {name: "Max Palvsky Commons", val: "neutral"}
+	"bartlett": {name: "Bartlett Dining Commons", val: "happy"},
+	"maxp": {name: "Max Palevsky Commons", val: "neutral"}
 };
 
 function getInfo(n) {
-	return "<h3>" + n.name + "</h3><h4>" + n.val + "<h4>";
+	return "<h3>" + n.name + "</h3><h4>" + n.val + "</h4>";
 };
 
 var mansueto = rsr.ellipse(411.775, 284.582, 30.927, 16.541);
 mansueto.transform("m-0.1795 -0.9838 0.9838 -0.1795 205.7261 740.7498");
 mansueto.attr({fill: ColorKey[dic["mansueto"].val], class: "building", id: 'mansueto'});
-mansueto.data({'info': dic['mansueto']}); 
+mansueto.data({'info': dic['mansueto']});
 regions.push(mansueto);
 
-var regenstein = rsr.path("M425.501,274.501l6.667-8.667v-20.667h15v-13.333h14.667v-10.333h16.333v13.333h26.333 v-3h7v-6.333h7v43h-6v5.333h-3v9.667h5.333v36.333h-4.333v-5.667h-4.333v-2.667h-2.667v-9.333h-3.667v-17.667h-21.333v11.667 c0,0,0,0-15.667,0v5.667c0,0,0,0-15.667,0v6.333h-15.333v-31.667l-4,4.667L425.501,274.501z"); 
+var regenstein = rsr.path("M425.501,274.501l6.667-8.667v-20.667h15v-13.333h14.667v-10.333h16.333v13.333h26.333 v-3h7v-6.333h7v43h-6v5.333h-3v9.667h5.333v36.333h-4.333v-5.667h-4.333v-2.667h-2.667v-9.333h-3.667v-17.667h-21.333v11.667 c0,0,0,0-15.667,0v5.667c0,0,0,0-15.667,0v6.333h-15.333v-31.667l-4,4.667L425.501,274.501z");
 regenstein.attr({fill: ColorKey[dic["regenstein"].val], class: "building", id: 'regenstein'});
 regenstein.data({'info': dic['regenstein']});
 regions.push(regenstein);
 
-var bartlett = rsr.path("M589.833,265.501v14.667h3.667v4.414h3.333v13.586h-2.667v5h-4v15.333h-3.333v8h-15.667v-8 h-2.667v-21h-4.333v-10.667h3.667c0-21.333,0-21.333,0-21.333l0,0H589.833z"); 
+var bartlett = rsr.path("M589.833,265.501v14.667h3.667v4.414h3.333v13.586h-2.667v5h-4v15.333h-3.333v8h-15.667v-8 h-2.667v-21h-4.333v-10.667h3.667c0-21.333,0-21.333,0-21.333l0,0H589.833z");
 bartlett.attr({fill: ColorKey[dic["bartlett"].val], class: "building", id: 'bartlett'});
-bartlett.data({'info': dic['bartlett']}); 
+bartlett.data({'info': dic['bartlett']});
 regions.push(bartlett);
 
-var maxp3 = rsr.path("M 541.167,200.167 567.667,200.167 567.667,193 574.5,193 574.5,235.667 570.5,235.667 570.5,229.333 537,229.333 537,244.833 588.5,244.833 588.5,240.168 590.833,240.168 590.833,181.502 541.188,181.502 z"); 
+var maxp3 = rsr.path("M 541.167,200.167 567.667,200.167 567.667,193 574.5,193 574.5,235.667 570.5,235.667 570.5,229.333 537,229.333 537,244.833 588.5,244.833 588.5,240.168 590.833,240.168 590.833,181.502 541.188,181.502 z");
 maxp3.attr({fill: ColorKey[dic["maxp"].val], class: "building", id: 'maxp'});
-maxp3.data({'info': dic['maxp']}); 
+maxp3.data({'info': dic['maxp']});
 regions.push(maxp3);
 
-var maxp2 = rsr.rect(442.5, 181.502, 84.25, 17.083); 
+var maxp2 = rsr.rect(442.5, 181.502, 84.25, 17.083);
 maxp2.attr({fill: ColorKey[dic["maxp"].val], class: "building", id: 'maxp'});
-maxp2.data({'info': dic['maxp']}); 
+maxp2.data({'info': dic['maxp']});
 regions.push(maxp2);
 
-var maxp = rsr.path("M430,181.502v17.083h-22.25v23.75H405v5.25h-7.5v-5.25h-5v-40.833H430z"); 
+var maxp = rsr.path("M430,181.502v17.083h-22.25v23.75H405v5.25h-7.5v-5.25h-5v-40.833H430z");
 maxp.attr({fill: ColorKey[dic["maxp"].val], class: "building", id: 'maxp'});
-maxp.data({'info': dic['maxp']}); 
+maxp.data({'info': dic['maxp']});
 regions.push(maxp);
 
-document.getElementById('region-name').innerHTML = 'Try hovering over a building!'; 
+document.getElementById('region-name').innerHTML = 'Try hovering over a building!';
 
 for (var i = 0; i < regions.length; i++){
-	
+
 	regions[i].mouseover(function(e){
 		this.node.style.opacity = 0.7;
 		this.node.style.stroke = 'yellow';
@@ -73,4 +73,3 @@ for (var i = 0; i < regions.length; i++){
 	});
 
 }
-
--- unit_tests/jasmine-standalone-3.0.0/spec/HelloWorldSpec.js
+++ unit_tests/jasmine-standalone-3.0.0/spec/HelloWorldSpec.js
@@ -1,7 +1,66 @@
-describe("Hello World", function(){ 
-   
-   it("should Return Hello world",function(){ 
-      expect(helloworld()).toEqual('Hello World'); 
-   }); 
+describe("Hello World", function(){
 
+   it("should Return Hello world",function(){
+      expect(helloworld()).toEqual('Hello World');
+   });
+
+   let mansueto = {name: "Mansueto Library", val: "happiest"};
+   //console.log(getInfo(mansueto));
+/*   it("should Return mansueto label", function(){
+     expect(getInfo(mansueto))
+      .toEqual('<h3>Mansueto Library</h3><h4>happiest</h4>');
+   });
+*/
 });
+
+
+
+/*
+test ideas
+
+database queries will return a logical location with an avg. score (1-10)
+test cases for query results
+firstCase = [];
+secondCase = [{logical-loc: 'bj', average: 6.1}];
+thirdCase = [{logical-loc: 'mansueto', average: 6.7},
+  {logical-loc: 'regenstein', average: 3.4},
+  {logical-loc: 'reynolds', average: 8.7},
+  {logical-loc: 'harper', average: 7.0},
+  {logical-loc: 'maxp', average: 7.0}
+  {logical-loc: 'booth', average: 0}];
+
+
+test the getInfo function
+let mansueto = {name: "Mansueto Library", val: "happiest"};
+let bartlett = {name: "Barlett Dining Commons", val: "happy"};
+getInfo(mansueto) = "<h3>Mansueto Library</h3><h4>happiest</h4>"
+getInfo(bartlett) = "<h3>Barlett Dining Commons</h3><h4>happy</h4>"
+getInfo(null) = null;
+
+test the ColorKey
+ColorKey[dic["mansueto"].val] = "#182825"
+ColorKey["neutral"] = "#E8E1EF"
+ColorKey["none"] = "#767676"
+
+test mapping of logical-locs in database to fullnames
+getName(thirdCase[0]) = "Mansueto Library"
+getName(thirdCase[1]) = "Regenstein Library"
+getName(secondCase[0]) = "Burton Judson Courts"
+getName(thirdCase[4]) = "Max Palevsky Commons"
+
+test function to calculate happiness text
+getHappinessRating(thirdCase[4]) = "happy"
+getHappinessRating(secondCase[0]) = "happy"
+getHappinessRating(thirdCase[2]) = "happiest"
+getHappinessRating(thirdCase[1]) = "sad"
+getHappinessRating(thirdCase[5]) = "none";
+
+
+
+
+
+
+
+
+
+*/
