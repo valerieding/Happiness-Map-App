28cfaf9df4a584b7b9d76714c363ac843c784c46
Claire Dolin <cdolin@uchicago.edu>
2018-02-09 18:42:30 -0600

    unit tests for map

---
+++ src/map/map.js
@@ -0,0 +1,21 @@
+//where map stuff will go
+
+var helloworld = function(){
+   return 'Hello World';
+};
+
+//map page functions
+function databaseToMapObj(n) {return n};
+
+function getInfo(n) { return n};
+
+function getName(n) { return n};
+
+function computeColor(n) { return n};
+
+function getHappinessRating(n) {return n};
+
+//querying database functions
+function getCampusScore() {return 0};
+
+function getAllBuildingScores() {return 0};
