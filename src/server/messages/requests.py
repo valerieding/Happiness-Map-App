2cd3594d4884b0804756fc9969b250c7c3f7b169
Mihai <mihai.andreescu995@gmail.com>
2018-02-13 16:47:05 -0600

    Server now no longer requires /request/issue_user_id

--- src/server/messages/requests.py
+++ src/server/messages/requests.py
@@ -1,11 +1,11 @@
 import logging
 
-from flask import Blueprint, request, jsonify
+from flask import Blueprint
 
 from server import DATABASE_MANAGER
 from server.database.message_api import MessageAPI
 from server.messages.forms import *
-from server.util import Location, validate_request
+from server.util import Location, generate_response
 from server.util.user import get_user_id
 
 message_requests = Blueprint('message_requests', __name__)
@@ -16,37 +16,42 @@ logger = logging.getLogger('messages_requests')
 
 @message_requests.route('/request/get_recent_posts', methods=['POST'])
 def get_recent_posts():
-    form = GetRecentPostsForm(request.form)
-    return validate_request(form, logger) or jsonify(
-        messageAPI.get_recent_posts(Location.from_request(form), form.start_time.data, form.end_time.data))
+    def response(form):
+        return messageAPI.get_recent_posts(Location.from_request(form), form.start_time.data, form.end_time.data)
+
+    return generate_response(GetRecentPostsForm, response, logger)
 
 
 @message_requests.route('/request/get_trending_posts', methods=['POST'])
 def get_trending_posts():
-    form = GetTrendingPostsForm(request.form)
-    return validate_request(form, logger) or jsonify(
-        messageAPI.get_trending_posts(Location.from_request(form)))
+    def response(form):
+        return messageAPI.get_trending_posts(Location.from_request(form))
+
+    return generate_response(GetTrendingPostsForm, response, logger)
 
 
 @message_requests.route('/request/add_post', methods=['POST'])
 def add_post():
-    form = AddPostForm(request.form)
-    return validate_request(form, logger, requires_valid_user_id=True) or jsonify(
-        messageAPI.add_post(get_user_id(), Location.from_request(form), form.message.data, form.reply_to.data))
+    def response(form):
+        return messageAPI.add_post(get_user_id(), Location.from_request(form), form.message.data, form.reply_to.data)
+
+    return generate_response(AddPostForm, response, logger)
 
 
 @message_requests.route('/request/upvote', methods=['POST'])
 def upvote():
-    form = UpvoteForm(request.form)
-    return validate_request(form, logger, requires_valid_user_id=True) or jsonify(
-        messageAPI.upvote(get_user_id(), form.post_id.data))
+    def response(form):
+        return messageAPI.upvote(get_user_id(), form.post_id.data)
+
+    return generate_response(UpvoteForm, response, logger)
 
 
 @message_requests.route('/request/downvote', methods=['POST'])
 def downvote():
-    form = DownvoteForm(request.form)
-    return validate_request(form, logger, requires_valid_user_id=True) or jsonify(
-        messageAPI.downvote(get_user_id(), form.post_id.data))
+    def response(form):
+        return messageAPI.downvote(get_user_id(), form.post_id.data)
+
+    return generate_response(DownvoteForm, response, logger)
 
 
 # TODO: activate this and have some sort of admin verification
