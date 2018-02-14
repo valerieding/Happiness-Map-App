c83721bb478082a57cd5c18d07711f5a07130ea6
Claire Dolin <cdolin@uchicago.edu>
2018-02-14 12:44:19 -0600

    finished my fillDB function, puts 200 random votes into the system!

--- src/static/map/fillDatabase.js
+++ src/static/map/fillDatabase.js
@@ -1,49 +1,7 @@
 // function just for testing, adds a bunch of votes to the database for a nice visualization
-function populateDB() {
-  $.ajax({
-    url: '/request/add_vote',
-    type: 'post',
-    dataType: 'json',
-    data: {'logical_location': 'regenstein', 'happiness_level': 1},
-  });
-  $.ajax({
-    url: '/request/add_vote',
-    type: 'post',
-    dataType: 'json',
-    data: {'logical_location': 'regenstein', 'happiness_level': 2},
-  });
-  $.ajax({
-    url: '/request/add_vote',
-    type: 'post',
-    dataType: 'json',
-    data: {'logical_location': 'regenstein', 'happiness_level': 2},
-  });
-  $.ajax({
-    url: '/request/add_vote',
-    type: 'post',
-    dataType: 'json',
-    data: {'logical_location': 'regenstein', 'happiness_level': 2},
-  });
-  $.ajax({
-    url: '/request/add_vote',
-    type: 'post',
-    dataType: 'json',
-    data: {'logical_location': 'mansueto', 'happiness_level': 3},
-  });
-  $.ajax({
-   url: '/request/add_vote',
-   type: 'post',
-   dataType: 'json',
-   data: {'logical_location': 'maxp', 'happiness_level': 5},
- });
- $.ajax({
-   url: '/request/add_vote',
-   type: 'post',
-   dataType: 'json',
-   data: {'logical_location': 'bartlett', 'happiness_level': 4},
- });
+function fillDB() {
  let i = 0;
- for (i = 0; i < 10; i++){
+ for (i = 0; i < 200; i++){
    let vote = Math.ceil(Math.random() * 5);
    let keys = Object.keys(FullNameKey);
    let place = keys[Math.floor(keys.length * Math.random())];
@@ -51,7 +9,8 @@ function populateDB() {
      url: '/request/add_vote',
      type: 'post',
      dataType: 'json',
-     data: {'logical_location': place, 'happiness_level': vote},
+     data: {'latitude': 0, 'longitude': 0,
+     'logical_location': place, 'happiness_level': vote},
    });
   }
 
