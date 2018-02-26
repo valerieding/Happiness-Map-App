fbe2212f098448c615a4b6409d39907dca45d8db
Claire Dolin <cdolin@uchicago.edu>
2018-02-26 13:05:48 -0600

    moved to statFunctions

---
+++ src/static/stats/statFunctions.js
@@ -0,0 +1,121 @@
+var dateCallback = function(value, index, values) {
+    let temp = new Date(1000 * value).toDateString();
+    return temp;
+}
+var timeCallback = function(value, index, values) {
+    let temp = new Date(1000 * value).toLocaleTimeString();
+    return temp;
+}
+
+
+
+function getUsersVotes(start_time) {
+  var myScores;
+  if (!start_time) {
+    start_time = 0;
+  }
+  $.ajax({
+    url: '/request/get_recent_votes',
+    type: 'post',
+    dataType: 'json',
+    async: false,
+    ata: {'start_time': start_time},
+    success: function(data){
+      myScores = data;
+    }
+  });
+  return myScores;
+}
+
+function getVoteHistory(start_time) {
+  var voteHistory = getUsersVotes(start_time);
+  var voteHistory2 = [];
+
+  for (var i = 0; i < Math.min(100, voteHistory.length); i++) {
+    let x = new Date(1000 * voteHistory[i].timestamp).toDateString();
+    voteHistory2.push({
+      x: voteHistory[i].timestamp,
+      y: voteHistory[i].happiness_level});
+  }
+  return voteHistory2;
+}
+
+function makeChart(ctx) {
+  var chart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        datasets: [{
+          label: "Vote",
+          backgroundColor: '#DD1C77',
+          borderColor: '#DF65B0',
+          borderWidth: 2,
+          fill: false,
+          lineTension: 0,
+          data: []
+        }]
+      },
+      options: {
+          scales: {
+              xAxes: [{
+                  type: 'linear',
+                  position: 'bottom',
+                  ticks: {
+                      callback: dateCallback
+                  }
+              }],
+              yAxes: [{
+                  type: 'linear',
+                  ticks: {
+                    beginAtZero: true,
+                    stepSize: 1
+                  }
+              }]
+          },
+          legend: {
+              display: false
+          },
+          tooltips: {
+              enabled: true,
+              callbacks: {
+                  title: function(tooltipItem, chart) {
+                      let temp = new Date(1000 * tooltipItem[0].xLabel);
+                      return temp.toLocaleString();
+                  }
+              }
+          }
+      }
+  });
+  return chart;
+}
+
+function updateChart(chart, start_time) {
+  ds = getVoteHistory(start_time);
+  if (ds.length > 3) {
+    document.getElementById("userVotesOverTime").style.display="block";
+    chart.data.datasets.forEach((d) => {
+          d.data = ds;
+      });
+    chart.update();
+  } else {
+    document.getElementById("userVotesOverTime").style.display="none";
+    document.getElementById("warningNotEnoughInfo").innerHTML = "Not Enough Data to Display"
+  }
+}
+
+function setScatterButtonFunctions() {
+  $('#optAllScatter').on('change', function () {
+    updateChart(chart, null);
+  });
+  $('#optWeekScatter').on('change', function () {
+    let now = Math.floor(Date.now() / 1000);
+    updateChart(chart, now - week);
+  });
+  $('#optDayScatter').on('change', function () {
+    let now = Math.floor(Date.now() / 1000);
+    updateChart(chart, now - day);
+  });
+  $('#optHourScatter').on('change', function () {
+    let now = Math.floor(Date.now() / 1000);
+    updateChart(chart, now - twoHr);
+  });
+}
