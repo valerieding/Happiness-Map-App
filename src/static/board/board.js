cc5bdfd21e7cfa691e3a032b097aaa7cb63d9873
Xavi <xzambrana@uchicago.edu>
2018-02-27 22:41:39 -0600

    loc recents

--- src/static/board/board.js
+++ src/static/board/board.js
@@ -9,6 +9,31 @@ $(document).ready(function(){
         document.getElementById('myform').innerHTML = "";
       }
       welcomeText(happyL);
+      for (let key in log_locs) {
+        $("#loc_drop").append('<option value=' + key + '>' + log_locs[key] + '</option>');
+      }
+      $("#vote_btn").click(function(){
+        var drop_loc;
+        var map_lock;
+        var happy;
+        var lock;
+        if(currentMapLoc == null) {
+          drop_loc = $("#loc_drop option:selected").val();
+          if (drop_loc === 'blank') {
+            drop_loc = '';
+            getRecents();
+          }
+          map_loc = $("map_loc").val();
+          //alert("map loc = " + map_loc);
+
+          happy = $("input[name=happiness_level]:checked").val();
+
+          // for the time being
+          loc = drop_loc;
+          getRecents(loc);
+
+        }
+      })
 });
 
 //Change 'welcome' text based off the user's most recent happiness level vote.
@@ -273,3 +298,54 @@ function timeSince(timeStamp) {
         return '~' + Math.round(elapsed/msPerYear ) + ' years ago';   
     }
 }
+
+
+var log_locs = Object.freeze({
+  blank: "",
+  offcampus: "Off Campus",
+  alumni : "Alumni House",
+  bartlett : "Bartlett Dining Commons",
+  bond : "Bond Chapel",
+  bookstore : "University Bookstore",
+  booth : "Booth School of Business",
+  bj : "Burton Judson Courts",
+  cathey : "Cathey Dining Commons",
+  classicsBuilds : "Classics Quad Buildings",
+  cobb : "Cobb Hall",
+  crerar : "Crerar Library",
+  eastBuild : "East Quad Buildings",
+  edward : "Edward H Levi Hall",
+  harper : "Harper Memorial Library",
+  harris : "Harris School",
+  henry : "Henry Crown Field House",
+  hutch : "Hutchinson Commons",
+  ida : "Ida Noyes",
+  lab : "Lab Buildings",
+  law : "Law School",
+  logan : "Logan and Midway Studios",
+  mainNorth : "Main Quad North Buildings",
+  mansueto : "Mansueto Library",
+  maxp : "Max Palevsky Commons",
+  med : "Medical Campus",
+  multi : "Office of Multicultural Affairs",
+  new_grad : "New Graduate Residence Halls",
+  north : "Campus North Residence and Dining",
+  northBuild : "North Quad Buildings",
+  oriental : "Oriental Institute",
+  physics : "Physics Buildings",
+  quadr : "Quadrangle Club",
+  ratner : "Ratner Athletics Center",
+  regenstein : "Regenstein Library",
+  reynolds : "Reynolds Club",
+  rock : "Rockefeller Chapel",
+  rosenwald : "Rosenwald",
+  ryeck : "Ryerson/Eckhart",
+  saieh : "Saieh Hall for Economics",
+  smart : "Smart Museum",
+  snell :  "Snell-Hitchcock",
+  ssr : "Social Science Research",
+  stuart : "Stuart Hall",
+  south : "South Campus Residence Hall",
+  southeast : "South East Quad Buildings",
+  swift : "Swift Hall",
+}); 
