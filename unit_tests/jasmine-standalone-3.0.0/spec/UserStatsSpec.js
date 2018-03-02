f22e6e20f457fef6c2fa6693733022a1d5f70631
agonzalez200 <agonzalez8@uchicago.ed>
2018-03-01 21:22:07 -0600

    removed calcAvgVote tests

--- unit_tests/jasmine-standalone-3.0.0/spec/UserStatsSpec.js
+++ unit_tests/jasmine-standalone-3.0.0/spec/UserStatsSpec.js
@@ -65,37 +65,4 @@ describe("User Stats Tests", function(){
       });
     });
   });
-
-	let votes1 = [];
-	let votes2 = [2];
-	let votes3 = [-1];
-	let votes4 =[10];
-	let votes5 = [2.3];
-	let votes6 = [1,2,3,-5,4];
-
-
-	// tests for version of calcAvgVote() function that is easily testible without
-	// needing to make asynchronous queries during testing
-	// calcAvgVote() can still be tested with Acceptance Testing scenarios
-  
-	describe("calcAvgVote()", function(){
-		it("should return the average of a given list of happiness votes (integers 1-5)" +
-		   "If any invalid happiness votes are found, return average of any valid votes",
-		function(){
-			expect(calcAvgVote(votes1).toEqual(0));
-				// valid, no votes
-				// In final implementation, voting page will immediately load
-				// If user visits the 'Stats' page prior to voting, this will be their avg vote
-			expect(calcAvgVote(votes2).toEqual(2));
-				// valid, 0 < happiness_level <= 5
-			expect(calcAvgVote(votes3)).toEqual(0);
-				// invalid, 0 < happiness_level <= 5
-			expect(calcAvgVote(votes4)).toEqual(0);
-				// invalid, 0 < happiness_level <= 5
-			expect(calcAvgVote(votes5)).toEqual(0);
-				// invalid, happiness_level must be an integer
-			expect(calcAvgVote(votes6)).toEqual(2.5);
-				// valid, average of [1,2,3,4] is 2.5
-		});
-	});
 });
